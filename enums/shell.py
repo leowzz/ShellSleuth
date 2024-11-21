1adbde0211789fa854a29f354134c55f56e8c3d1
WangZhanze <wangzhanze@huoban.ai>
2024-11-21 17:49:51 +0800

    放弃解析shell方案, 将要改为配置文件

--- enums/shell.py
+++ enums/shell.py
@@ -4,16 +4,20 @@ from enum import Enum
 class ShellType(str, Enum):
     BASH = "bash"
     ZSH = "zsh"
+    CMD = "cmd"
+    POWERSHELL = "powershell"
 
 
 class ShellConfigFileName(str, Enum):
-    BASH = ".bashrc"
-    ZSH = ".zshrc"
+    BASH = "~/.bashrc"
+    ZSH = "~/.zshrc"
+    POWERSHELL = "$PROFILE"
 
 
 def get_config_file_name_by_shell(shell: ShellType) -> ShellConfigFileName:
     shell_type_2_config_file_name = {
         ShellType.BASH: ShellConfigFileName.BASH,
         ShellType.ZSH: ShellConfigFileName.ZSH,
+        ShellType.POWERSHELL: ShellConfigFileName.POWERSHELL
     }
     return shell_type_2_config_file_name[shell]
